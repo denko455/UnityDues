0d5c2f58cbacf287f748526f64b9281b875776c3
denko455 <31391511+denko455@users.noreply.github.com>
2024-05-04 17:05:11 +0200

    Promene

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('')
             ->login()
-            ->passwordReset()
+            ->passwordReset(null)
             ->profile(EditProfile::class)
             ->breadcrumbs(false)
             ->colors([
